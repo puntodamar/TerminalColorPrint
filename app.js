d45fd748454a8dd94fe9ccfca3e9dbd70fe6d803
Unknown <punto.damar2@gmail.com>
2019-03-05 13:16:56 +0700

    read json from file

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const validator = require('validator');
 const chalk     = require('chalk');
+const yargs     = require('yargs');
+const fs        = require('fs');
+
 const getNotes  = require('./notes');
 
-const msg = getNotes();
-const greenMsh = chalk.red('Success!');
-console.log(greenMsh);
+yargs.version('1.1.0');
+// create a command
+yargs.command({
+    command: 'add',
+    descibe: 'Add a new note',
+    builder: {
+        title: {
+            describe    : 'Note title',
+            demandOption: true,
+            type        : 'string'
+        },
+        body: {
+            describe    : 'Note body',
+            demandOption: true,
+            type        : 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log(`Adding a new note \ntitle : ${argv.title} \ncontent : ${argv.body}`);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing a note');
+    }
+})
+
+yargs.parse();
